550b2705f9e97d378fe2df9385d2f67bbac4aca3
ngawang-zangphel <ngawangzangphel360@gmail.com>
2024-10-14 10:37:31 +0600

    use get details

---
+++ server/controllers/userController.js
@@ -0,0 +1,32 @@
+const router = require('express').Router();
+const User = require('./../models/user');
+const authMiddleware = require('../middlewares/authMiddleware');
+
+//get Details.
+//normal
+// router.get('get-logged-user', async (req, res) => { })
+//but since this route should be accessible to authenticated users only thus we need to use our authMiddleware.
+//the reason autheMiddleware is between is becuase we need to call this function before executing async callback function.
+// router.get('get-logged-user', authMiddleware, async (req, res) => { })
+
+//to get the user details
+// in AuthMiddleare, we are extracting userId from the jwt token and then sending it as request to this callback function
+router.get('/get-logged-user', authMiddleware, async (req, res) => {
+    try {
+        const user = await User.findOne({_id: req.body.userId});
+        res.send({
+            message: 'User details fetched successfully',
+            success: true,
+            user: user
+        })
+
+    } catch (err) {
+        res.send({
+            message: err.message,
+            success: false
+        })
+    }
+})
+
+module.exports = router;
+
--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -5,9 +5,7 @@ module.exports  = (req, res, next) => {
 
         const token  = req.headers.authorization.split(' ')[1];
         const decodedToken = jwt.verify(token, process.env.SECRET_KEY);
-
         req.body.userId = decodedToken.userId;
-
         next();
 
     } catch (err) {
