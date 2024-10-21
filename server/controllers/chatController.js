26e37ff552b0e29b648a3fddf4387547f6843b01
ngawang-zangphel <ngawangzangphel360@gmail.com>
2024-10-21 20:40:38 +0600

    get all chats for a currently logged in user

--- server/controllers/chatController.js
+++ server/controllers/chatController.js
@@ -23,4 +23,25 @@ router.post('/create-new-chat', authMiddleware, async (req, res) => {
     }
 });
 
+//Get Route
+router.get('/get-all-chats', authMiddleware, async (req, res) => {
+    try {
+        //$in: check if current logged in user id
+        //Anny chat whose members Id contains userID of the currently logged in user
+        const allChats = await Chat.find({members: {$in: req?.body?.userId}});
+
+        res.status(200).send({
+            message: "Chat Fetched successfully",
+            succcess: true,
+            data: allChats
+        });
+
+    }catch (err) {
+        res.status(404).send({
+            message: err.message,
+            succcess: false
+        })
+    }
+});
+
 module.exports = router;
