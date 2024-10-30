b5790cf87faad5ca7d58c9eb42ad3d079fc1df94
ngawang-zangphel <ngawangzangphel360@gmail.com>
2024-10-30 11:30:09 +0600

    login page

--- client/src/pages/login/index.js
+++ client/src/pages/login/index.js
@@ -1,5 +1,51 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
 function Login() {
-    return <h2>THis is a Login page</h2>
+
+    const [user, setUser] = React.useState({
+        email: '',
+        password: ''
+    });
+
+    function onFormSubmit(event) {
+        event.preventDefault();
+        // TODO: Send user data to server for registration
+        console.log(user);
+    };
+
+    return (
+        <div className="container">
+            <div className="container-back-img"></div>
+            <div className="container-back-color"></div>
+            <div className="card">
+                <div className="card_title">
+                    <h1>Login Here</h1>
+                </div>
+                <div className="form" onSubmit={onFormSubmit}>
+                    <form>
+                        <input 
+                            type="email" placeholder="Email"
+                            value={user?.email}
+                            onChange={(e) => setUser({...user, email: e.target.value })}
+
+                        />
+                        <input 
+                            type="password" placeholder="Password"
+                            value={user?.password}
+                            onChange={(e) => setUser({...user, password: e.target.value })}
+                        />
+                        <button>Login</button>
+                    </form>
+                </div>
+                <div className="card_terms">
+                    <span>Don't have an account yet? 
+                        <Link to="/signup">Signup Here</Link>
+                    </span>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Login;
--- client/src/pages/signup/index.js
+++ client/src/pages/signup/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function SignUp() {
 
@@ -59,7 +60,7 @@ function SignUp() {
                 </div>
                 <div className="card_terms">
                     <span>Already have an account?
-                        <a href="#">Login Here</a>
+                        <Link to="/login">Login Here</Link>
                     </span>
                 </div>
             </div>
